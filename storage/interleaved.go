941ef69289557f40ad6dae126043696080e25dfe
Yubing Zhang <joseph.zyb@gmail.com>
2026-03-13 19:17:29 +1100

    feat: implement storage system and data ingestion logic

--- storage/interleaved.go
+++ storage/interleaved.go
@@ -1,1 +1,23 @@
 package storage
+
+import (
+	"fmt"
+
+	"github.com/teleohead/frida-das/pkg/frida"
+)
+
+type InterleavedSlab struct {
+	Data []frida.Scalar
+	Size int
+}
+
+func (s *InterleavedSlab) GetBatch(index int) ([]frida.Scalar, error) {
+	start := index * frida.BatchSize
+	end := start + frida.BatchSize
+
+	if end > s.Size {
+		return nil, fmt.Errorf("batch index %d out of bounds for slab size %d", index, s.Size)
+	}
+
+	return s.Data[start:end], nil
+}
