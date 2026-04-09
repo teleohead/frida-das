e7d7044b3688ef6093a6c899e53fa1aa956dd4fa
Yubing Zhang <joseph.zyb@gmail.com>
2026-04-09 15:46:01 +1000

    feat: define simulation config, simulation result structs, and helper methods for outputting result

--- sim/config.go
+++ sim/config.go
@@ -1,1 +1,77 @@
 package sim
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/teleohead/frida-das/pkg/frida"
+)
+
+type SimConfig struct {
+	Params           frida.FriParams
+	Data             []byte
+	NumNodes         int
+	SamplesPerNode   int
+	NetworkWorkers   int // number of concurrent goroutines, defaults to GOMAXPROCS
+	CorruptPositions []int
+}
+
+type SimResult struct {
+	Config      SimConfig    `json:"config"`
+	NodeResults []NodeResult `json:"node_results"`
+
+	TotalSampled    int  `json:"total_sampled"`
+	TotalAccepted   int  `json:"total_accepted"`
+	TotalRejected   int  `json:"total_rejected"`
+	Reconstructable bool `json:"reconstructable"`
+
+	CommitTimeNs    time.Duration `json:"commit_time_ns"`
+	AvgProofTimeNs  time.Duration `json:"avg_proof_time_ns"`  // time it takes for an Open() call
+	AvgVerifyTimeNs time.Duration `json:"avg_verify_time_ns"` // time it takes for an Verify() call
+
+	SingleProofBytes int           `json:"single_proof_bytes"`
+	SamplingTimeNs   time.Duration `json:"sampling_time_ns"`
+	Throughput       float64       `json:"throughput_sps"` // unit of measurement: samples per second
+}
+
+func (r *SimResult) String() string {
+	return fmt.Sprintf(
+		"========================================\n"+
+			"           SIMULATION SUMMARY           \n"+
+			"========================================\n"+
+			"Nodes:           %d\n"+
+			"Samples/Node:    %d\n"+
+			"Reconstructable: %v\n"+
+			"Total Accepted:  %d\n"+
+			"Total Rejected:  %d\n"+
+			"Throughput:      %.2f SPS\n"+
+			"Commit Time:     %s\n"+
+			"Avg Proof Time:  %s\n"+
+			"Avg Verify Time: %s\n"+
+			"Proof Size:      %d bytes\n"+
+			"Total Sim Time:  %s\n"+
+			"========================================",
+		len(r.NodeResults), r.Config.SamplesPerNode,
+		r.Reconstructable,
+		r.TotalAccepted, r.TotalRejected,
+		r.Throughput,
+		r.CommitTimeNs, r.AvgProofTimeNs, r.AvgVerifyTimeNs,
+		r.SingleProofBytes, r.SamplingTimeNs,
+	)
+}
+
+// ExportJSON marshals a SimResult and writes the data to disk.
+// The data can be then used for the final report.
+func (r *SimResult) ExportJSON(filepath string) error {
+	fileData, err := json.MarshalIndent(r, "", "  ")
+	if err != nil {
+		return fmt.Errorf("failed to marshal result: %w", err)
+	}
+	err = os.WriteFile(filepath, fileData, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to write file: %w", err)
+	}
+	return nil
+}
