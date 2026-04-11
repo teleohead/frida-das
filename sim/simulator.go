97d686973186c8527f4e2ba7aec8f365e24336fd
Yubing Zhang <joseph.zyb@gmail.com>
2026-04-11 15:28:01 +1000

    feat: simulator (verify logic pending)

--- sim/simulator.go
+++ sim/simulator.go
@@ -1,1 +1,155 @@
 package sim
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+
+	"github.com/teleohead/frida-das/internal/prover"
+	"github.com/teleohead/frida-das/pkg/frida"
+)
+
+func RunSimulation(cfg SimConfig) (*SimResult, error) {
+	if cfg.NetworkWorkers == 0 {
+		cfg.NetworkWorkers = runtime.GOMAXPROCS(0)
+	}
+
+	// COMMIT
+	tCommitStart := time.Now()
+
+	builder := prover.NewBuilder(cfg.Params)
+	commitment, proverState, err := builder.CommitAndProve(cfg.Data)
+
+	if err != nil {
+		return nil, fmt.Errorf("commit failed: %w", err)
+	}
+
+	commitDuration := time.Since(tCommitStart)
+
+	domainSize := proverState.DomainSize
+	receptionNeeded := domainSize / cfg.Params.BlowupFactor
+
+	// SINGLE PROOF METRICS
+	proofSampleCount := 10
+	if proofSampleCount > domainSize {
+		proofSampleCount = domainSize
+	}
+
+	var totalProofDuration time.Duration
+	var singleProofBytes int
+
+	for i := 0; i < proofSampleCount; i++ {
+		pos := i % domainSize
+		start := time.Now()
+		proof, err := prover.Open(proverState, []int{pos})
+		totalProofDuration += time.Since(start)
+		if err != nil {
+			return nil, fmt.Errorf("proof measurement at pos %d: %w", pos, err)
+		}
+		if i == 0 {
+			singleProofBytes = measureProofSize(proof)
+		}
+	}
+
+	avgProofDuration := totalProofDuration / time.Duration(proofSampleCount)
+
+	// NETWORK SETUP
+	numRequests := cfg.NumNodes * cfg.SamplesPerNode
+	requestChan := make(chan SampleRequest, numRequests)
+	resultChan := make(chan NodeResult, cfg.NumNodes)
+
+	var dp DataProvider
+
+	if len(cfg.CorruptPositions) > 0 {
+		dp = NewMaliciousProvider(cfg.CorruptPositions)
+	} else {
+		dp = NewHonestProvider()
+	}
+
+	net := NewNetwork(proverState, dp, requestChan, cfg.NetworkWorkers)
+
+	nodes := make([]LightNode, cfg.NumNodes)
+
+	for i := range nodes {
+		nodes[i] = LightNode{
+			ID:          i,
+			NumSamples:  cfg.SamplesPerNode,
+			DomainSize:  domainSize,
+			Commitment:  commitment,
+			Params:      cfg.Params,
+			RequestChan: requestChan,
+			ResultChan:  resultChan,
+		}
+	}
+
+	// EXEC
+	samplingStartTime := time.Now()
+
+	net.Start()
+
+	for i := range nodes {
+		go nodes[i].Run()
+	}
+
+	nodeResults := make([]NodeResult, cfg.NumNodes)
+	for i := 0; i < cfg.NumNodes; i++ {
+		nodeResults[i] = <-resultChan
+	}
+
+	close(requestChan)
+
+	net.Wait()
+
+	samplingDuration := time.Since(samplingStartTime)
+
+	// ANALYSIS
+	totalSampled := 0
+	totalAccepted := 0
+	totalRejected := 0
+	coverageSet := make(map[int]bool, numRequests)
+
+	for _, nr := range nodeResults {
+		totalSampled += len(nr.SampledPositions)
+		totalAccepted += nr.AcceptedCount
+		totalRejected += nr.RejectedCount
+		for _, pos := range nr.SampledPositions {
+			coverageSet[pos] = true
+		}
+	}
+
+	reconstructable := len(coverageSet) >= receptionNeeded
+
+	var throughput float64
+	if samplingDuration > 0 {
+		throughput = float64(totalSampled) / samplingDuration.Seconds()
+	}
+
+	// TODO: VERIFIER LOGIC
+	var avgVerifyDuration time.Duration
+
+	return &SimResult{
+		Config:           cfg,
+		NodeResults:      nodeResults,
+		TotalSampled:     totalSampled,
+		TotalAccepted:    totalAccepted,
+		TotalRejected:    totalRejected,
+		Reconstructable:  reconstructable,
+		CommitTimeNs:     commitDuration,
+		AvgProofTimeNs:   avgProofDuration,
+		AvgVerifyTimeNs:  avgVerifyDuration,
+		SingleProofBytes: singleProofBytes,
+		SamplingTimeNs:   samplingDuration,
+		Throughput:       throughput,
+	}, nil
+
+}
+
+func measureProofSize(proof *frida.FriProof) int {
+	size := 0
+	for _, layer := range proof.Layers {
+		for _, path := range layer.Paths {
+			size += len(path.LeafValue) + len(path.Siblings) + frida.HashBytes + frida.BytesPerElement
+		}
+	}
+	return size
+}
